29e04f011eda0432b6bb3fd2b5158a8e5337d52f
kegaub <kevin.gauthier.22.06@gmail.com>
2021-03-09 17:58:07 -0500

    Update _exercice_version_prof.py

--- _exercice_version_prof.py
+++ _exercice_version_prof.py
@@ -8,13 +8,11 @@ from exercice_ch6 import frequence
 from turtle import *
 import re
 
-
 def compute_volume_and_mass(a=2, b=4, c=6, masse_vol=10):
     volume = math.pi * a * b * c * 4 / 3
     masse = volume * masse_vol
 
     return volume, masse
-
 
 def draw_branch(branch_len, pen_size, angle):
     if branch_len > 0 and pen_size > 0:
